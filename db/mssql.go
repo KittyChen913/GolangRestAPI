1a642069c5ead37a391042e9e3519488fdd83816
KittyChen913 <nightmare.kitty111@gmail.com>
2025-06-30 15:36:50 +0800

    db add retry mechanism

--- db/mssql.go
+++ db/mssql.go
@@ -3,19 +3,33 @@ package db
 import (
 	"database/sql"
 	"os"
+	"time"
 
 	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/microsoft/go-mssqldb"
 )
 
 var Db *sql.DB
+var retryCount int = 3
 
 func InitDb() {
 	var err error
 	Db, err = sql.Open("sqlserver", os.Getenv("DemoDb"))
 	if err != nil {
-		panic("Unable to connect to the database.")
+		panic("Unable to parse database config: " + err.Error())
 	}
 	Db.SetMaxOpenConns(10)
 	Db.SetMaxIdleConns(5)
+
+	// 連線測試
+	for i := 1; i <= retryCount; i++ {
+		err = Db.Ping()
+		if err == nil {
+			break
+		}
+		if i == retryCount {
+			panic("Unable to connect to the database: " + err.Error())
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
