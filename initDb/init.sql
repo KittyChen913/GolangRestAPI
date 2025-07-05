f59c4861a1e8b45187493cfa1f15e763abb766cd
KittyChen913 <nightmare.kitty111@gmail.com>
2025-07-06 02:09:38 +0800

    add docker-compose.yml, add mssql service

---
+++ initDb/init.sql
@@ -0,0 +1,24 @@
+-- create DB
+CREATE DATABASE DemoDb;
+GO
+
+USE DemoDb;
+GO
+-- create [Admins] tables
+CREATE TABLE Admins (
+    AdminId tinyint IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
+    Password VARCHAR(500) NOT NULL,
+    Email NVARCHAR(200)
+);
+GO
+
+-- create [Users] tables
+CREATE TABLE Users (
+    Id tinyint IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
+    Age INT NULL,
+    Email NVARCHAR(200),
+    CreateDateTime DATETIME NULL DEFAULT GETDATE()
+);
+GO
